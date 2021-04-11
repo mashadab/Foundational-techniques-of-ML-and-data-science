ac56cd3b51b6d70d23e8a02b62b1e675919225eb
mashadab@mit.edu <afzalshadab0@yahoo.com>
2021-04-11 00:29:42 -0500

    Clustering

--- Homework4-Clustering/7.4_kmeansalgo.py
+++ Homework4-Clustering/7.4_kmeansalgo.py
@@ -11,149 +11,76 @@ import matplotlib.pyplot as plt
 import copy
 
 #k-means algorithm
+#According to question
 p = 0.7; q = 0.3
-
-'''
 Asub = p*np.ones((50,50))
 I    = np.eye(3)
 A    = np.kron(I,Asub)
 A[A==0] = q 
-'''
-
-A = 0.3*np.ones((150,150), dtype = float)
-A[:50,:50] = 0.7
-A[50:100, 50:100] = 0.7
-A[100:,100:] = 0.7
 
 B = np.random.uniform(0,1,(150,150))
-
-'''
-for i in range(np.shape(A)[0]):     #columns
-    for j in range(np.shape(A)[1]): #rows
-        if (B[i,j]>=A[i,j]):
-            A[i,j] = 1.0
-        else:
-            A[i,j] = 0.0
-'''
 
 plt.figure()
 plt.imshow(A)
 plt.title('Initial A')
-print('Initial nonzero',np.count_nonzero(A))
-A = B > A
+A = B > A 
 
 plt.figure()
 plt.imshow(A)
 plt.title('Filtered A')
-print('Filtered nonzero',np.count_nonzero(A))
 
-def permute_rows_cols(A):
-    A = np.random.permutation(A) #randomly permutating the elements of A in rows
-    A = np.transpose(A) #transposing to do the permutation in first index
-    A = np.random.permutation(A) #randomly permutating the elements of A in columns
-    A = np.transpose(A)  #transposing to do get the final permuted matrix
-    return A
-
-A = permute_rows_cols(A)
+random_permutation = np.random.permutation(150)
 
 plt.figure()
-plt.imshow(A)
-plt.title('Permuted A')
+A_permuted = A[:,random_permutation]
+A_permuted = A_permuted[random_permutation,:]
+plt.imshow(A_permuted)
+plt.title("Permuted and filtered Matrix A")
 
-#x = np.argwhere(A==1)[:,0]; y=np.argwhere(A==1)[:,1] 
-df = pd.DataFrame({
-        'x':np.argwhere(A==1)[:,0],
-        'y':np.argwhere(A==1)[:,1] 
-})
-np.random.seed(1000)
+def kmeans(data, k, random_permutation, max_iters = 10000):
 
-k = 10
-#centroids[i] = [x,y]
-centroids = {
-        i+1: [np.random.randint(0,150),np.random.randint(0,150)]
-        for i in range(k)
-} 
+    ''' 
+    data: nxd matrix 
+    k: number of clusters
+    random_permutation: The indices of how data was shuffled. We will unshuffle the labels using this.
+    max_iters: maximum iterations of the algorithm
+    '''
+    n = data.shape[0]
+    d = data.shape[1]
+    data = data.astype(float)
+    centres = np.random.uniform(size = (k, d))
+    labels = np.zeros((n), dtype = float)
 
-fig = plt.figure(figsize=(10,10))
-plt.scatter(df['x'],df['y'],color='k')
-colmap = {1: 'r', 2: 'g', 3: 'b', 4: 'c', 5: 'm', 6: 'y', 7: 'darkblue', 8: 'orange', 9: 'deeppink', 10: 'maroon'}
-for i in centroids.keys():
-    plt.scatter(*centroids[i],color=colmap[i])
-plt.xlim(0,150)
-plt.ylim(0,150)
-plt.show()
+    for iters in range(max_iters):
 
-#Assignment stage
-def assignment(df,centroids):
-    for i in centroids.keys():
-        #sqrt((x1 - x2)^2 - (y1 - y2)^2)
-        df['distance_from_{}'.format(i)] = (
-            np.sqrt(
-                    (df['x'] - centroids[i][0]) **2.0
-                    + (df['y'] - centroids[i][1]) **2.0
-                    )
-        )
-    centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
-    df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
-    df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
-    df['color']   = df['closest'].map(lambda x: colmap[x])
-    return df
+        labels_old = np.copy(labels)
+        for i in range(n):
+            distances = np.linalg.norm(centres - data[i,:], axis = 1)
+            labels[i] = np.argmin(distances)
 
-df = assignment(df, centroids)
-print(df.head())
-
-fig = plt.figure(figsize=(10,10))
-plt.title('Initialization')
-plt.scatter(df['x'],df['y'], color = df['color'], alpha=0.3, edgecolor='k')
-for i in centroids.keys():
-    plt.scatter(*centroids[i], color=colmap[i],s=200)
-plt.xlim(0,150)
-plt.ylim(0,150)
-plt.show()
-
-#Update Stage
-old_centroids = copy.deepcopy(centroids)
-
-def update(k):
-    for i in centroids.keys():
-        centroids[i][0] = np.mean(df[df['closest'] == i]['x'])
-        centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
-    return k
-
-centroids = update(centroids)
-
-fig = plt.figure(figsize=(10,10))
-plt.title('Update')
-ax  = plt.axes()
-plt.scatter(df['x'],df['y'], color = df['color'], alpha=0.3, edgecolor='k')
-for i in centroids.keys():
-    plt.scatter(*centroids[i], color=colmap[i],s=200)
-plt.xlim(0,150)
-plt.ylim(0,150)
-
-for i in old_centroids.keys():
-    old_x = old_centroids[i][0]
-    old_y = old_centroids[i][1]
-    dx = (centroids[i][0] - old_centroids[i][0])*0.75
-    dy = (centroids[i][1] - old_centroids[i][1])*0.75
-    ax.arrow(old_x, old_y, dx, dy, head_width=2, head_length=3, fc = colmap[i], ec= colmap[i])
-plt.show()
-
-#Repeat until convergence
-while True:
-    closest_centroids = df['closest'].copy(deep=True)
-    centroids = update(centroids)
-    df = assignment(df,centroids)
-    if closest_centroids.equals(df['closest']):
-        break
-fig = plt.figure(figsize=(10,10))
-plt.title('Final Result')
-plt.scatter(df['x'],df['y'], color = df['color'], alpha=0.3, edgecolor='k')
-for i in centroids.keys():
-    plt.scatter(*centroids[i], color=colmap[i],s=200)
-plt.xlim(0,150)
-plt.ylim(0,150)
-plt.show()   
-
-
+        for i in range(k):
+            centres[i,:] = np.mean(np.squeeze(data[np.argwhere(labels==i),:]), dtype=float, axis = 0)
         
+        if (np.all(labels_old == labels)):
+            print("K means converged.")
+            break
+
+    sum_of_squares = 0.
+    unshuffled_labels = np.zeros((n), dtype = int)
+    for i in range(n):
+        unshuffled_labels[random_permutation[i]] = labels[i]
+        sum_of_squares = sum_of_squares + np.linalg.norm(A_permuted[i,:]-centres[int(labels[i])])**2
+
+    return unshuffled_labels, sum_of_squares
+
+unshuffled_labels_3, _ = kmeans_n(A_permuted, 3, random_permutation)
+plt.figure()
+plt.scatter(np.arange(150), unshuffled_labels_3, color='r')
+plt.title("Labels for unscrambled rows of A for k = 3")
+
+
+k_array = np.arange(1,11)
+sum_of_squares_array = np.zeros((10))
+for i in range(10):
+    unshuffled_labels, sum_of_squares_array[i] = kmeans_n(A_permuted, i+1, random_permutation)
+    print(np.unique(unshuffled_labels, return_counts=True), sum_of_squares_array[i])
